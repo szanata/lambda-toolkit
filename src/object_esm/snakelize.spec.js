466d5433959903543418af71a160dc8213bdbd53
Stéfano Zanata <221676+szanata@users.noreply.github.com>
2026-02-06 22:23:48 +0000

    refactor: migration string to esm module (#34)
    Co-authored-by: Stéfano Zanata <221676+szanata@users.noreply.github.com>

---
+++ src/object_esm/snakelize.spec.js
@@ -0,0 +1,60 @@
+import { snakelize } from './snakelize.js';
+import { describe, it } from 'node:test';
+import { deepStrictEqual, strictEqual } from 'node:assert';
+
+describe( 'Object: Snakelize', () => {
+  it( 'Should return a copy the object where each key was snakelize', () => {
+    const obj = {
+      propTree: {
+        ALL_CAPS: 1,
+        deepProp: {
+          snake_case: 'bar',
+          camelCase: 'foo'
+        }
+      },
+      anArray: [ 1, { innerArrayProp: 1 }, 3 ],
+      prop3WithNumber: 1,
+      mixedCAMELContent: 1,
+      PascalCase: 1,
+      shallowProp: 'test'
+    };
+
+    const result = snakelize( obj );
+
+    deepStrictEqual( result, {
+      prop_tree: {
+        all_caps: 1,
+        deep_prop: {
+          snake_case: 'bar',
+          camel_case: 'foo'
+        }
+      },
+      an_array: [ 1, { inner_array_prop: 1 }, 3 ],
+      prop3_with_number: 1,
+      mixed_camel_content: 1,
+      pascal_case: 1,
+      shallow_prop: 'test'
+    } );
+  } );
+
+  it( 'Should keep ALL_CAPS properties if using the option', () => {
+    const obj = {
+      propTree: {
+        ALL_CAPS: 1
+      }
+    };
+
+    const result = snakelize( obj, { keepAllCaps: true } );
+
+    deepStrictEqual( result, {
+      prop_tree: {
+        ALL_CAPS: 1
+      }
+    } );
+  } );
+
+  it( 'Should not break on null object', () => {
+    const result = snakelize( null );
+    strictEqual( result, null );
+  } );
+} );
