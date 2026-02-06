84ec65b68d15e666c8342d76ca078aaf15ea733b
Stéfano Zanata <szanata@users.noreply.github.com>
2026-02-06 23:30:02 +0000

    fix: Fixing eslint config and math module

--- src/math_esm/calc_z_score.spec.js
+++ src/math_esm/calc_z_score.spec.js
@@ -8,11 +8,11 @@ describe( 'Math: Calc Z Score Spec', () => {
     const mean = 20;
     const std = 10;
     const result = calcZScore( sample, mean, std );
-    strictEqual( result, -1 )
+    strictEqual( result, -1 );
   } );
 
   it( 'Should return NaN if the std is 0', () => {
     const result = calcZScore( 1, 4, 0 );
-    strictEqual( result, NaN )
+    strictEqual( result, NaN );
   } );
 } );
