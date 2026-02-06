19ed242ee91f7e2ded62da28a138cf9f56bae34e
Stéfano Zanata <szanata@users.noreply.github.com>
2026-02-06 22:00:52 +0000

    refactor: migration string to esm module

---
+++ src/string_esm/index.js
@@ -0,0 +1,9 @@
+import { camelize } from './camelize.js';
+import { capitalizeWords } from './capitalize_words.js';
+import { snakelize } from './snakelize.js';
+
+export {
+  camelize,
+  capitalizeWords,
+  snakelize
+};
