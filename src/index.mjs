11a2c342210eafe545aa5b8eadc1ee1ea17fa313
Stéfano Zanata <221676+szanata@users.noreply.github.com>
2026-03-04 22:41:43 +0000

    feat: Adding esm and cjs webpack builds (#48)
    Co-authored-by: Stéfano Zanata <szanata@users.noreply.github.com>

---
+++ src/index.mjs
@@ -0,0 +1,9 @@
+export * as LambdaApi from './lambda_api_esm/index.js';
+export * as array from './array/index.js';
+export * as aws from './aws/index.js';
+export * as epoch from './epoch/index.js';
+export * as math from './math/index.js';
+export * as object from './object/index.js';
+export * as redis from './redis/index.js';
+export * as string from './string/index.js';
+export * as utils from './utils/index.js';
