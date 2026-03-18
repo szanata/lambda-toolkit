cdd039d3226212d33ea343a7efa2349e0bb4f107
Stéfano Zanata <szanata@users.noreply.github.com>
2026-03-18 23:28:25 +0000

    chore: Renaming .spec.js to .test.js to comply with node native test tool, improving package.json

--- src/epoch/days.spec.js
+++
@@ -1,9 +0,0 @@
-import { days } from './days.js';
-import { describe, it } from 'node:test';
-import { strictEqual } from 'node:assert';
-
-describe( 'Days Spec', () => {
-  it( 'Should return x days in ms', () => {
-    strictEqual( days( 3 ), 2.592e+8 );
-  } );
-} );
