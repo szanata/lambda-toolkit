4053dd4c9d37c68b22e7d40c3d3e0732d29ec842
lzanatta <46678005+lzanatta@users.noreply.github.com>
2026-03-22 20:14:44 -0300

    feat: generate .d.ts file

--- ops/build.sh
+++ ops/build.sh
@@ -21,4 +21,27 @@ npm run test
 printf "\e[0;36m(Bundling)\e[0m\n"
 npm run build
 
+printf "\e[0;36m(Generating types)\e[0m\n"
+
+npx -p typescript tsc \
+  --allowJs --checkJs false --strict false \
+  --module nodenext --target ES2024 \
+  --skipLibCheck --declaration --emitDeclarationOnly \
+  --declarationDir ./dts-out \
+  src/index.js
+
+echo '{
+  "compilerOptions": {
+    "target": "ES2024",
+    "typeRoots": []
+  }
+}' > ./dts-out/tsconfig.json
+
+npx dts-bundle-generator \
+  --out-file dist/index.d.ts \
+  --project ./dts-out/tsconfig.json \
+  ./dts-out/index.d.ts
+
+rm -rf ./dts-out
+
 printf "\e[0;36m(Done)\e[0m\n"
