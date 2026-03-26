25a72727d2cd5281ec46a90aa4ccfccd22dd4d81
Stéfano Zanata <221676+szanata@users.noreply.github.com>
2026-03-26 22:07:00 +0000

    chore: Adding pnpm as the new package manager

--- ops/publish.sh
+++ ops/publish.sh
@@ -9,21 +9,6 @@ printf "╭─────────╮\n"
 printf "│ \e[0;35mPublish\e[0;35m │\n"
 printf "╰─────────╯\e[0m\n"
 
-printf "\e[0;35m(Resolving Versions)\e[0m\n"
-
-pkg_name=$(node -p "require('./package.json').name")
-local_version=$(node -p "require('./package.json').version")
-remote_version=$(npm view "$pkg_name@$local_version" version 2>/dev/null || echo "unpublished")
-package="$pkg_name@$local_version"
-
-printf "Local Version is $local_version\n"
-
-if [ "$local_version" = "$remote_version" ]; then
-  printf "Version $local_version is already published, no actions necessary\n"
-else
-  printf "\e[0;35m(Publishing...)\e[0m\n"
-  npm publish
-  printf "$package published"
-fi
+pnpm publish -r --no-git-checks --verbose
 
 printf "\e[0;35m(Done)\e[0m\n"
