4c82cf60f1400ddefb009a6e7dacefac2634f685
Lorenzo Zanatta <46678005+lzanatta@users.noreply.github.com>
2026-03-26 19:47:18 -0300

    feat: dynamo.remove with native args (#63)
    * feat: dynamo.remove with native args
    
    * bump version

--- src/aws/dynamo/remove.js
+++ src/aws/dynamo/remove.js
@@ -1,10 +1,19 @@
 import { DeleteCommand } from '@aws-sdk/lib-dynamodb';
 
-export const remove = async ( client, tableName, key ) => {
-  const { Attributes: item } = await client.send( new DeleteCommand( {
+const parseArgs = args => {
+  // native args mode
+  if ( args[0] instanceof Object ) {
+    return args[0];
+  }
+  // sugar mode
+  return {
     ReturnValues: 'ALL_OLD',
-    TableName: tableName,
-    Key: key
-  } ) );
-  return item;
+    TableName: args[0],
+    Key: args[1]
+  };
+};
+
+export const remove = async ( client, ...args ) => {
+  const response = await client.send( new DeleteCommand( parseArgs( args ) ) );
+  return response.Attributes;
 };
