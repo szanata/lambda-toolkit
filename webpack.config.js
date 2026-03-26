5bb613cd3c74dc25e8f857d4b547d5eac6f66207
lzanatta <46678005+lzanatta@users.noreply.github.com>
2026-03-26 11:36:38 -0300

    fix: webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 import { resolve } from 'node:path';
+import { rmSync } from 'node:fs';
 
 const __dirname = import.meta.dirname;
 const distFolder = resolve( __dirname, 'dist' );
+
+rmSync( distFolder, { recursive: true, force: true } );
 
 const config = {
   context: __dirname,
@@ -22,8 +25,7 @@ export default [
       filename: 'index.mjs',
       library: {
         type: 'module'
-      },
-      clean: true
+      }
     },
     experiments: {
       outputModule: true
@@ -36,8 +38,7 @@ export default [
       filename: 'index.cjs',
       library: {
         type: 'commonjs2'
-      },
-      clean: true
+      }
     }
   }
 ];
