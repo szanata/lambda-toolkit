99c5ca918a28fb73b8024f1349c4bf4e72cb465a
lzanatta <46678005+lzanatta@users.noreply.github.com>
2026-06-11 22:05:26 -0300

    feat: new S3 functions

--- src/aws/s3/get_signed_url.js
+++ src/aws/s3/get_signed_url.js
@@ -1,8 +1,6 @@
-import { getSignedUrl as getSignedUrlS3 } from '@aws-sdk/s3-request-presigner';
-import { GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedDownloadUrl } from './get_signed_download_url.js';
 
-export const getSignedUrl = async ( client, bucket, key, expiration ) => {
-  const getObjectCmd = new GetObjectCommand( { Bucket: bucket, Key: key } );
-  const url = await getSignedUrlS3( client, getObjectCmd, { expiresIn: expiration } );
-  return url;
-};
+/**
+ * @deprecated Use the getSignedDownloadUrl function instead
+ */
+export const getSignedUrl = async ( client, bucket, key, expiration ) => getSignedDownloadUrl( client, bucket, key, expiration );
