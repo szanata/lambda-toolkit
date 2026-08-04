6479a2605667dad25c3ac228e52a01bda5a6ca1e
lzanatta <46678005+lzanatta@users.noreply.github.com>
2026-08-04 13:22:14 -0300

    feat: Support native args on S3 download

--- src/aws/s3/get_signed_download_url.js
+++ src/aws/s3/get_signed_download_url.js
@@ -1,8 +1,12 @@
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { GetObjectCommand } from '@aws-sdk/client-s3';
 
-export const getSignedDownloadUrl = async ( client, bucket, key, expiration ) => {
-  const getObjectCmd = new GetObjectCommand( { Bucket: bucket, Key: key } );
+export const getSignedDownloadUrl = async ( client, bucket, key, expiration, nativeArgs ) => {
+  const getObjectCmd = new GetObjectCommand( {
+    ...nativeArgs,
+    Bucket: bucket,
+    Key: key
+  } );
   const url = await getSignedUrl( client, getObjectCmd, { expiresIn: expiration } );
   return url;
 };
